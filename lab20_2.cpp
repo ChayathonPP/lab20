fdd4f7060eb859ef5462fabd67371b632940d6d0
Chayathon <chayathon_p@cmu.ac.th>
2023-02-21 20:32:39 +0700

    lab20_2 COMPLETE

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -5,8 +5,8 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(const Rect* R1, const Rect* R2) {
+    double dx = min(R1->x + R1->w, R2->x + R2->w) - max(R1->x, R2->x);
+    double dy = min(R1->y, R2->y) - max(R1->y - R1->h, R2->y - R2->h);
+    return (dx > 0 && dy > 0) ? dx * dy : 0;
 }
